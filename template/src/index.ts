a7b8a9ff71ac826103d3a7fe15ab695ed43b6dac
Shreshth Mohan <sremog@gmail.com>
2025-08-21 14:23:48 +0530

    add team for cloudflare access

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,6 +3,7 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
 	readonly: true,
+  cfAccessTeamName: 'washi-program',
 	// basicAuth: {
 	//   username: 'username',
 	//   password: 'password'
